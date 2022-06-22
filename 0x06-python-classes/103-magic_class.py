7a0d8081ce33cb0218911ed27f726f2ba1bc25b0
TanakaMasunda <masundatanaka@gmail.com>
2022-06-21 20:19:13 -0700

    classes an pbjects

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ class MagicClass:
         """initializes the data"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius is not a number")
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
