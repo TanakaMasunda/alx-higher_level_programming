1b8d57ff633194707ec987eb2aeacc60ce5b2bfa
TanakaMasunda <masundatanaka@gmail.com>
2022-06-21 18:36:09 -0700

    objects and classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class square with size private attribute """
+
+
+class Square:
+
+    def __init__(sel, square_size):
+        """to construct the square"""
+        self.__ = square_size
