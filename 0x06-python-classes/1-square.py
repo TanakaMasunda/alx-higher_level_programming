14b228f72b600a95ea6492a48c0b67b3ca32495b
TanakaMasunda <masundatanaka@gmail.com>
2022-06-21 18:47:43 -0700

    objects and classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,6 @@
 class Square:
     """ class to create a square with size"""
 
-    def __init__(sel, square_size):
+    def __init__(self, square_size):
         """to construct the square"""
         self.__size = square_size
