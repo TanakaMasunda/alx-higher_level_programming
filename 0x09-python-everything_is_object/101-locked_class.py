6795920ced48e8210c835631f34045343e15ce84
TanakaMasunda <masundatanaka@gmail.com>
2022-06-28 13:46:00 -0700

    objects

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,8 @@
 class LockedClass:
     """
     with no class or object attribute, that prevents the user from dynamically
-    creating new instance attributes, except if the new instance attribute is called first_name
+    creating new instance attributes, except if the new instance attribute is
+    called first_name
     """
 
     __slots__ = ["first_name"]
