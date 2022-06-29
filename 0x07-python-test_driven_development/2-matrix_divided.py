247df550e64087044ef8a42ab4f07d8f1c54fa12
TanakaMasunda <masundatanaka@gmail.com>
2022-06-29 15:40:39 -0700

    tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,14 +13,15 @@ def matrix_divided(matrix, div):
         TypeError: If the matrix contains rows of different sizes.
         TypeError: If div is not an int or float.
         ZeroDivisionError: If div is  zero(0).
-        Returns: A new matrix representing the result of the division.
+    Returns:
+        A new matrix representing the result of the division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(elem, int) or isinstance(elem, float))
-                for elem in [num for row in matrix for num in row])):
+                    for elem in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
-                "integers/floats")
+                        "integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -32,6 +33,5 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     new_matrix = ([list(map(lambda x: round(x / div, 2), row))
-        for row in matrix])
-
+                  for row in matrix])
     return new_matrix
