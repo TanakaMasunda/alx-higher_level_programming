551d04ba1d734a0f7e4430532d4aa56bfc6f6874
TanakaMasunda <masundatanaka@gmail.com>
2022-07-05 16:27:09 -0700

    input/output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,19 +3,21 @@
 """
 
 
-def __init__(self, first_name, last_name, age):
-    """Initialize new student in class
+class Student:
+    """representation of a student"""
 
-    Args:
-        first_name(string) is the first name of the student
-        last_name(string) is the last name of the student
-        age(integer) is the age of the student
-    """
-    self.first_name = first name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        """Initialize new student in class
 
+        Args:
+            first_name(string) is the first name of the student
+            last_name(string) is the last name of the student
+            age(integer) is the age of the student
+        """
+        self.first_name = first name
+        self.last_name = last_name
+        self.age = age
 
-def to_json(self):
-    """that retrieves a dictionary representation of a Student instance"""
-    return self.__dict__
+    def to_json(self):
+        """that retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
