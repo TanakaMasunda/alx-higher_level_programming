48c370b5d1f421affa616159be96f46db0b61048
TanakaMasunda <masundatanaka@gmail.com>
2022-07-06 20:07:35 -0700

    inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ def add_attribute(cls, name, value):
     """
         add new attributes when possible
     """
-    if hasattr(cls, "__dict__"):
+    if hasattr(cls, '__dict__'):
         setattr(cls, name, value)
     else:
         raise TypeError("cant add new attribute")
