80c3a0f438d56350eef59f42e931df8de10b2d0c
TanakaMasunda <masundatanaka@gmail.com>
2022-08-09 14:43:53 -0700

    intro to sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---dlt database hbtn_0c_0,scrpt shldnt fail if t dnt exst,no(SELECt/SHOW)statmnt
+-- dlt datbase hbtn_0c_0,scrpt shldnt fail if t dnt exst,no(SELECt/SHOW)statmnt
 DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
---lists all the tables of a database,will b passed as argument of mysql commnd
+-- list all the tables of a database,will b passed as argument of mysql commnd
 SHOW TABLES;
