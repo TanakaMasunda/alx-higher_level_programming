005d8940b73694d77883b7fffe42502580a7f8bb
TanakaMasunda <masundatanaka@gmail.com>
2022-08-09 14:16:30 -0700

    intro to sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---create database hbtn_0c_0,scrpt shldnt fail f t exst,no(SELECT/SHOW)statement
-CREATE DATABASE IF NOT EXISTS 'hbtn_0c_0';
+--Create database hbtn_0c_0,scrpt shldnt fail f t exst,no(SELECT/SHOW)statement
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
