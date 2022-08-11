06163ae522402318384edae09ee2363b73d11d30
TanakaMasunda <masundatanaka@gmail.com>
2022-08-11 14:56:45 -0700

    more sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create MySQL servr usr(user_0d_1.)wth(all privileges,user_0d_1_pwd as pswd)
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES ON * . * TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
