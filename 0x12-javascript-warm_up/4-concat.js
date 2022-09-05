6bb1abfd1010867ebcdb603801cdf78af9c5e80c
TanakaMasunda <masundatanaka@gmail.com>
2022-09-05 16:49:32 -0700

    js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log('java' + ' is ' + 'script');
+console.log(process.argv[2] + ' is ' + process.argv[3]);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const strlist = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(strlist[i]);
+}
