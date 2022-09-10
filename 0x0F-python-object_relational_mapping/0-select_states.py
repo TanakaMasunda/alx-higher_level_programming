2ce6f89d68bec1c3b31f2ebfa3afaacf2451125e
TanakaMasunda <masundatanaka@gmail.com>
2022-09-10 15:33:04 -0700

    orms

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
 """
 script that lists all states from the database hbtn_0e_0_usa:3 arguments:
 mysql username, mysql password & database name (no argument validation needed
 (import MySQLdb),connect to a MySQL server running on localhost at port 3306
 sorted in ascending order by state,code shouldnt be executed when imported
 """
+
 
 import MySQLdb
 from sys import argv
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwrd=argv[2], db=argv[3])
+
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
