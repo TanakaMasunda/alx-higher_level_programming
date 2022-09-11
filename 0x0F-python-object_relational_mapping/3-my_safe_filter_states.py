d46b0b1aa414196c3c35bf1d07ece7dcbb2655fd
TanakaMasunda <masundatanaka@gmail.com>
2022-09-10 17:06:26 -0700

    orms

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument & display all values in the states table where
+name matches arg from the database hbtn_0e_0_usa:arguments(state name searched,
+mysql username, mysql password & database name (no argument validation needed
+(import MySQLdb),connect to a MySQL server running on localhost at port 3306
+sorted in ascending order by state,code shouldnt be executed when imported
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3], charset="utf8")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that lists all cities from the database hbtn_0e_0_usa:3 arguments:
+mysql username, mysql password & database name (no argument validation needed
+(import MySQLdb),connect to a MySQL server running on localhost at port 3306
+sorted in ascending order by cities.id,code shouldnt be executed when imported
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name\
+            FROM cities LEFT JOIN states\
+            ON states.id = cities.state_id\
+            ORDER BY cities.id ASC")
+    states = cur.fetchall()
+
+    for city in cities:
+        print(city)
