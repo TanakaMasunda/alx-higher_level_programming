23f5f36c457ee4821679130fe39adf960c916f33
TanakaMasunda <masundatanaka@gmail.com>
2022-09-10 18:21:23 -0700

    orms

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all State objects that contain the letter a from the database
+Lists all State objects from the database hbtn_0e_6_usa
 """
-
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
