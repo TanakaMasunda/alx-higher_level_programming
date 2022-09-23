88daa0506a4a75edec8cf6b5fab92a4e7c597775
TanakaMasunda <masundatanaka@gmail.com>
2022-09-22 17:35:23 -0700

    networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#  script that takes in a URL, sends request to URL & display size of body of the response
-
+# script take in URL, send request to URL & display size of body of the response
 curl -s http://www.tana.com:5000
