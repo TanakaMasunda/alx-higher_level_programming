37575e915d63464e52c84452cf142fa792727176
TanakaMasunda <masundatanaka@gmail.com>
2022-09-26 18:06:28 -0700

    networking

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""script that takes in a URL,send a request to the URL,display the body of the response (decoded in utf-8).
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
